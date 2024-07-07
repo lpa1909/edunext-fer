0e0ae9f8eb5a329ee7c45ae464fd4414667d2d4a
lpa1909 <anlphe176714@fpt.edu.vn>
2024-07-07 21:57:40 +0700

    fix database

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { FaUserGraduate, FaMapMarkerAlt, FaUserFriends } from 'react-icons/fa';
+import React, { useContext } from 'react';
+import { CourseContext } from '../Context/CourseContext';
 import '../Css/CourseCard.css';
 
-const CourseCard = ({ course }) => {
+const CourseCard = () => {
+  const { courses } = useContext(CourseContext);
+
   return (
-    <div className="course-card">
-      <h3>{course.title}</h3>
-      <div className="course-info">
-        <div className="course-detail">
-          <FaUserGraduate className="icon" />
-          <span className="instructor">{course.instructor}</span>
+    <div className='course-container'>
+      {courses.map(course => (
+        <div key={course.courseID} className="course-card">
+          <h2>{course.courseName}</h2>
+          <p><strong>Course Code:</strong> {course.courseCode}</p>
+          <p><strong>Class:</strong> {course.classID}</p>
+          <p><strong>Course Type:</strong> {course.courseType}</p>
+          <p><strong>Number of Students:</strong> {course.numOfStudents}</p>
+          <p><strong>Semester:</strong> {course.semester}</p>
         </div>
-        <div className="course-detail">
-          <FaMapMarkerAlt className="icon" />
-          <span className="location">{course.location}</span>
-        </div>
-        <div className="course-detail">
-          <FaUserFriends className="icon" />
-          <span className="students">{course.students} students</span>
-        </div>
-      </div>
-      <a href="#" className="course-link">Go to course &rarr;</a>
+      ))}
     </div>
   );
 };
